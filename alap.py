fcce7bc92a525c049d9462133514ad5f8cf6d5b3
DominikFulop <72389075+DominikFulop@users.noreply.github.com>
2022-01-19 14:13:24 +0100

    alap

---
+++ alap.py
@@ -0,0 +1,6 @@
+erő = int(input ('mennyire legyél erős: '))
+kitartó = int(input ('mennyire legyél kitartó: '))
+karizma = int(input ('mennyire legyél karizmatikus: '))
+türelem = int(input ('mennyire legyél türelmes: '))
+érzelem = int(input ('mennyire legyél érzelmes: '))
+
